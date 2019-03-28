f8ec2fe53d2e775210358b94bcb754adccf2253d
r0ann3l <roannel.fdez@gmail.com>
2019-03-28 09:49:22 -0400

    ADD: Submodule structure for samples.

---
+++ spring-boot-telegram-framework-samples/my-name/src/main/java/com/github/netkorp/myname/MyNameApplication.java
@@ -0,0 +1,13 @@
+package com.github.netkorp.myname;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyNameApplication.class, args);
+    }
+
+}
