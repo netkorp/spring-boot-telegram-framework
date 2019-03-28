d6482882371a1a51a05d413deefdf292e97fb068
r0ann3l <roannel.fdez@gmail.com>
2019-03-27 20:19:10 -0400

    ADD: Project structure.

---
+++ src/main/java/com/github/netkorp/telegram/framework/SpringBootTelegramFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.github.netkorp.telegram.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTelegramFrameworkApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootTelegramFrameworkApplication.class, args);
+  }
+
+}
