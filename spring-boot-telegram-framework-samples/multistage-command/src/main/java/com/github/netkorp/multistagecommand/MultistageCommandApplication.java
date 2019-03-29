a7ec540d73568c6138f753f7c289ab53b25e32e9
r0ann3l <roannel.fdez@gmail.com>
2019-03-29 17:10:02 -0400

    Multistage command sample.

---
+++ spring-boot-telegram-framework-samples/multistage-command/src/main/java/com/github/netkorp/multistagecommand/MultistageCommandApplication.java
@@ -0,0 +1,13 @@
+package com.github.netkorp.multistagecommand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultistageCommandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultistageCommandApplication.class, args);
+    }
+
+}
