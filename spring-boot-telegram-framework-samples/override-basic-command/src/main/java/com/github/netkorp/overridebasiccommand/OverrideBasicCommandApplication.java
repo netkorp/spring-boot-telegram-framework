1e545ef6dd60993baa14da6853d1efb920191dbc
r0ann3l <roannel.fdez@gmail.com>
2019-03-29 20:07:26 -0400

    Sample for overriding a basic command.

---
+++ spring-boot-telegram-framework-samples/override-basic-command/src/main/java/com/github/netkorp/overridebasiccommand/OverrideBasicCommandApplication.java
@@ -0,0 +1,13 @@
+package com.github.netkorp.overridebasiccommand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverrideBasicCommandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OverrideBasicCommandApplication.class, args);
+    }
+
+}
